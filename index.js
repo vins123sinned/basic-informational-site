ad1eb32aac54dc2b2e30b01f194cfce56249b798
Vinson 2 <vinsonhe2@gmail.com>
2025-08-15 18:43:21 -0700

    feat(server): serve index.html on site load

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('testing...')
+import http from 'node:http';
+import fs from 'node:fs';
+import url from 'node:url';
+
+const server = http.createServer((req, res) => {
+  fs.readFile('./index.html', 'utf8', (err, data) => {
+    if (err) {
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end('Server Error');
+      return;
+    }
+
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(data);
+  });
+});
+
+server.listen(8080);
