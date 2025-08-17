7cb676a132f4b8e0f6f4c77ca54743cd948518c2
Vinson 2 <vinsonhe2@gmail.com>
2025-08-16 17:02:30 -0700

    feat(server): implement routing functionality
    - Shows the correct page according to the url
    - Returns an error 404 page when visiting an invalid url
    - Returns an error 500 page when something is wrong with the server

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
-import http from 'node:http';
+import http from 'node:http'; 
 import fs from 'node:fs';
-import url from 'node:url';
 
 const server = http.createServer((req, res) => {
-  fs.readFile('./index.html', 'utf8', (err, data) => {
-    if (err) {
-      res.writeHead(500, { 'Content-Type': 'text/plain' });
-      res.end('Server Error');
-      return;
-    }
+  if (req.url !== '/favicon.ico') {
+    const filePath = (req.url === '/') ? '/index' : req.url;
+    fs.readFile(`.${filePath}.html`, 'utf8', (err, data) => {
+      if (err) {
+        if (err.code === 'ENOENT') {
+          fs.readFile('./404.html', 'utf8', (err, data) => {
+            if (err) {
+              res.writeHead(500, { 'Content-Type': 'text/plain' });
+              res.end('Server Error');
+              return;
+            }
 
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end(data);
-  });
+            res.writeHead(404, { 'Content-Type': 'text/html' });
+            res.end(data);
+          });
+          return;
+        } else {
+          res.writeHead(500, { 'Content-Type': 'text/plain' });
+          res.end('Server Error');
+          return;
+        }
+      }
+
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(data);
+    });
+  }
 });
 
 server.listen(8080);
